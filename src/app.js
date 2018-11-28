05cd8d37956181e672d926822a8bba446b8d79c0
Daniel Margalit <daniel.m@minutemedia.com>
2018-11-28 15:17:02 +0200

    Handling Form Data

--- src/app.js
+++ src/app.js
@@ -9,17 +9,24 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
 
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+)
 const accounts = JSON.parse(accountData)
 
-const userData = fs.readFileSync('src/json/users.json', 'utf8')
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'), 'utf8'
+)
 const users = JSON.parse(userData)
 
 app.get('/', (req, res) =>
   res.render('index', {
     title: 'Account Summary',
-    accounts: accounts,
+    accounts,
   })
 );
 
@@ -46,6 +53,45 @@ app.get('/profile', (req, res) =>
     user: users[0],
   })
 );
+
+app.get('/transfer', (req, res) =>
+  res.render('transfer')
+);
+
+app.get('/payment', (req, res) =>
+  res.render('payment', {
+    account: accounts.credit,
+  })
+);
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(
+    path.join(__dirname, 'json', 'accounts.json'),
+    accountsJSON,
+    'utf8'
+  )
+  res.render('transfer', {
+    message: "Transfer Completed",
+  })
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount
+  accounts.credit.available += parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(
+    path.join(__dirname, 'json', 'accounts.json'),
+    accountsJSON,
+    'utf8'
+  )
+  res.render('transfer', {
+    message: "Payment Successful",
+    account: accounts.credit
+  })
+});
 
 app.listen(3000, () =>
   console.log('PS Project Running on port 3000!')
